b1ce3598fbe93e89e2dff3639ee79ce5d2a0a6ee
Young Jeong <youngjeong46@gmail.com>
2022-01-25 17:55:58 -0800

    revised for eks sample app

--- aws_demo.sh
+++ aws_demo.sh
@@ -1,20 +1,74 @@
+cat << EOF > cluster.yaml
+---
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: ${CLUSTER_NAME}
+  region: ${AWS_REGION}
+  version: "1.21"
+
+iam:
+  withOIDC: true
+  serviceAccounts:
+  - metadata:
+      name: aws-load-balancer-controller
+      namespace: kube-system
+    wellKnownPolicies:
+      awsLoadBalancerController: true
+  - metadata:
+      name: ebs-csi-controller-sa
+      namespace: kube-system
+    wellKnownPolicies:
+      ebsCSIController: true
+  - metadata:
+      name: cluster-autoscaler
+      namespace: kube-system
+      labels: {aws-usage: "cluster-ops"}
+    wellKnownPolicies:
+      autoScaler: true
+
+managedNodeGroups:
+  - name: "node1"
+    minSize: 3
+    desiredCapacity: 4
+    maxSize: 5
+    volumeSize: 100
+    tags:
+      # EC2 tags required for cluster-autoscaler auto-discovery
+      k8s.io/cluster-autoscaler/enabled: "true"
+      k8s.io/cluster-autoscaler/testing: "owned"
+    instanceType: m5.xlarge
+    iam:
+      withAddonPolicies:
+        ebs: true
+        fsx: true
+        efs: true
+        cloudWatch: true
+
+EOF
+
 
 eksctl create cluster -f cluster.yaml
 
+curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.1/docs/install/iam_policy.json
+
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam-policy.json
+
 eksctl create iamserviceaccount \
-  --cluster=redis \
+  --cluster=$CLUSTER_NAME \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
-  --attach-policy-arn=arn:aws:iam::164382793440:policy/AWSLoadBalancerControllerIAMPolicy \
+  --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy \
   --override-existing-serviceaccounts \
   --approve
 
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
-  --set clusterName=gran \
+  --set clusterName=$CLUSTER_NAME \
   --set serviceAccount.create=false \
   --set serviceAccount.name=aws-load-balancer-controller 
 
 k apply -f kubernetes-manifests/
-
-k apply -f daemonset_gagent.yaml
